789429011b0e6e2048dd14f6b99eecca05055cf6
Sergio Mejia <sergio12mg25@gmail.com>
2025-09-23 10:47:47 -0500

    Actualización en la navegación y ajustes de lógica

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -13,11 +14,26 @@ import CrearDispositivo from './screens/CrearDispositivo';
 import Configuracion from './screens/Configuracion';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function InicioStackScreen({ agregarDispositivo, dispositivos }) {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="InicioPrincipal" options={{ headerShown: false }}>
+        {(props) => <Inicio {...props} dispositivos={dispositivos} />}
+      </Stack.Screen>
+      <Stack.Screen name="CrearDispositivo" options={{ title: 'Agregar Dispositivo' }}>
+        {(props) => <CrearDispositivo {...props} onAgregar={agregarDispositivo} />}
+      </Stack.Screen>
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
-  // Array para almacenar dispositivos
+  // Array para almacenar dispositivos (estado compartido entre los componentes Inicio.js y CrearDispositivo.js)
   const [dispositivos, setDispositivos] = useState([]);
 
+  // Agregar un dispositivo con nombre e imagen
   function agregarDispositivo(nombre, imagen) {
     const nuevoDispositivo = {
       id: Math.random().toString(),
@@ -49,11 +65,14 @@ export default function App() {
           },
           tabBarActiveTintColor: '#00C7CC',
           tabBarInactiveTintColor: 'gray',
+          headerShown: false, // Oculta el header que se pone por defecto para cada pantalla desde el tabNavigator
         })}
       >
-        <Tab.Screen name="Inicio" component={Inicio} options={{ title: 'Inicio' }} />
+        <Tab.Screen name="Inicio">
+          {/* Aquí se usa el componente Stack anidado */}
+          {(props) => <InicioStackScreen {...props} dispositivos={dispositivos} agregarDispositivo={agregarDispositivo} />}
+        </Tab.Screen>
         <Tab.Screen name="Controlador" component={Controlador} options={{ title: 'Controlador' }} />
-        <Tab.Screen name="Agregar dispositivo" component={CrearDispositivo} options={{ title: 'Agregar dispositivo' }}  />
         <Tab.Screen name="Configuracion" component={Configuracion} options={{ title: 'Configuración' }} />
       </Tab.Navigator>
     </NavigationContainer>
