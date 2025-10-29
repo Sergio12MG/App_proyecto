e7e6a996225b15a2df27eed4895a1b058db53687
Sergio Mejia <sergio12mg25@gmail.com>
2025-10-29 14:38:07 -0500

    El componente de Dispositivo obtiene la url de la imagen desde el servidor para renderizarla

--- components/DispositivoItem.js
+++ components/DispositivoItem.js
@@ -1,11 +1,15 @@
 import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 
+// NOTA: Asegúrate de que esta URL coincida con la de tu archivo base.js
+const BASE_URL = 'http://192.168.101.8:8000';
+
 export default function DispositivoItem({ nombre, imagen }) {
     return (
         <View style={styles.cont}>
             <Text>{ nombre }</Text>
+            {/* La imagen viene del servidor, por lo que se construye la URL completa */}
             <Image
-                source={{ uri: imagen }}
+                source={{ uri: `${BASE_URL}/${imagen}` }}
                 style={[styles.image, { resizeMode: 'cover' }]}
             ></Image>
         </View>
@@ -17,11 +21,16 @@ const styles = StyleSheet.create({
         borderColor: '#0BC9F4',
         borderWidth: 5,
         borderRadius: 15,
-        padding: 20,
-        margin: 15
+        padding: 15,
+        margin: 10,
+        flex: 1, // Permite que el elemento ocupe el espacio disponible
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     image: {
-        width: 150,
-        height: 150,
+        width: 70,
+        height: 70,
+        borderRadius: 15,
+        marginTop: 10,
     }
 });
